2532c89d22057da7f7a3967043c81131d5a18b72
emrealgan <alganemre.8@gmail.com>
2023-11-14 22:31:04 +0300

    message

--- LabPro2/src/prolab2/Person.java
+++ LabPro2/src/prolab2/Person.java
@@ -3,7 +3,7 @@ package prolab2;
 abstract public class Person {
 
 	public static void main(String[] args) {
-//		Bu classta Ad ve soyad bilgileri icermelidi
+//		Bu classta Ad ve soyad bilgileri icermelidir
 	}
 
 }
