c5107d3cff4cc0185756ee2835c08d5fefd45cd5
Enes Berkant Özer <enesberkantozer260@gmail.com>
2023-11-14 16:11:19 +0300

    Update Admin.java
    update  class

--- LabPro2/src/prolab2/Admin.java
+++ LabPro2/src/prolab2/Admin.java
@@ -3,6 +3,7 @@ package prolab2;
 public class Admin extends User{
 	public static void main(String args[]) {
 		System.out.println("Abuzettin");
+		System.out.println("NAber aga");
 // 		metotlar user'dan override edilecek
 // 		Admin panelinde kullanıcı adı ve ¸sifre ile giris yapılmalıdır. Sistemde sadece 1 adet admin bulunmalıdır.
 // 		Var olan firmalar g¨or¨unt¨ulenebilir.
