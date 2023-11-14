d77b77b9090af14926fb1ec3d9bd8e3701b436c2
Enes Berkant Özer <enesberkantozer260@gmail.com>
2023-11-14 20:50:33 +0300

    Update Admin.java
    Update

--- LabPro2/src/prolab2/Admin.java
+++ LabPro2/src/prolab2/Admin.java
@@ -1,9 +1,10 @@
 package prolab2;
 
+import java.sql.*;
+
 public class Admin extends User{
 	public static void main(String args[]) {
-		System.out.println("Abuzettin");
-		System.out.println("NAber Emre");
+//		Class.forName("com.mysql.cj.jdbc.Driver");
 // 		metotlar user'dan override edilecek
 // 		Admin panelinde kullanıcı adı ve ¸sifre ile giris yapılmalıdır. Sistemde sadece 1 adet admin bulunmalıdır.
 // 		Var olan firmalar g¨or¨unt¨ulenebilir.
