e3899f68d7299550fe14baccaa730eb4ed6d02c4
emrealgan <alganemre.8@gmail.com>
2023-11-28 11:55:31 +0300

    adding tickets and distances data, using map data type

--- LabPro2/src/prolab2/Route.java
+++ LabPro2/src/prolab2/Route.java
@@ -1,15 +1,22 @@
 package prolab2;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 public class Route {
 	
+    public static Map<String, Integer> distanceRoad = new HashMap<>();
+    public static Map<String, Integer> distanceRail = new HashMap<>();
+    public static Map<String, Integer> distanceAir = new HashMap<>();
+
 	private ArrayList<String> travel = new ArrayList<String>();
-	public ArrayList<ArrayList<String>> allRoute = new ArrayList<ArrayList<String>>(); 
-	
+	public ArrayList<Route> allRoute = new ArrayList<Route>();
+	public static int routeCount = 0;
+		
 	public Route(ArrayList<String> travel) {
 		this.travel = travel;
-		allRoute.add(travel);
+		allRoute.add(routeCount, this);
+		routeCount++;
 	}
 
 	public String getTravel(int x) {
@@ -19,38 +26,42 @@ public class Route {
 		travel.add(deger);
 	}
 
-	public String getAllRoute(int x, int y) {
-		 if (x >= 0 && x < allRoute.size() && y >= 0) {
-		        ArrayList<String> row = allRoute.get(x);
-
-		        // y koordinatının satırın sınırları içinde olup olmadığını kontrol et
-		        if (y < row.size()) {
-		            // Belirtilen koordinatlardaki elemanı al
-		            String element = row.get(y);
-		            return element;
-		        } 
-		        else 
-		            // y, satırın sınırlarının dışında ise işle
-		            return "Geçersiz y koordinatı";
-		 } 
-		 else 
-		        // x, ArrayList'in sınırlarının dışında ise işle
-		        return "Geçersiz x koordinatı";
-		    
-	}
-	public ArrayList<String> getAllRoute(int x) {
-		 if (x >= 0 && x < allRoute.size()) 
-			 return allRoute.get(x);
-		  
-		 else 
-		        // x, ArrayList'in sınırlarının dışında ise işle
-		        return new ArrayList<String>(List.of("Geçersiz x koordinatı"));
-		    
+	public Route getAllRoute(int x) { 
+		if(x < routeCount)
+			 return allRoute.get(x); 
+		else 
+			return null;
 	}
 	
 
 	public static void main(String[] args) {
 //		 Seferin kalkıs, varıs noktalarını ve sefere ait mesafe bilgilerini içermeli
+		distanceRoad.put("Istanbul-Kocaeli", 100);
+		distanceRoad.put("Istanbul-Eskisehir", 300);
+		distanceRoad.put("Istanbul-Ankara", 500);
+		distanceRoad.put("Istanbul-Konya", 600);
+		
+		distanceRail.put("Istanbul-Kocaeli", 75);
+		distanceRail.put("Istanbul-Bilecik", 225);
+		distanceRail.put("Istanbul-Eskisehir", 300);
+		distanceRail.put("Istanbul-Ankara", 375);
+		distanceRail.put("Istanbul-Konya", 450);
+		distanceRail.put("Kocaeli-Bilecik", 75);
+		distanceRail.put("Kocaeli-Eskisehir", 150);
+		distanceRail.put("Kocaeli-Ankara", 300);
+		distanceRail.put("Kocaeli-Konya", 350);
+		distanceRail.put("Bilecik-Eskisehir", 75);
+		distanceRail.put("Bilecik-Ankara", 225);
+		distanceRail.put("Bilecik-Konya", 300);
+		distanceRail.put("Eskisehir-Ankara", 150);
+		distanceRail.put("Eskisehir-Konya", 225);
+		
+		distanceAir.put("Istanbul-Ankara", 250);
+		distanceAir.put("Istanbul-Konya", 250);
+
+		
+
+
 	}
 
 }
--- LabPro2/src/prolab2/Trip.java
+++ LabPro2/src/prolab2/Trip.java
@@ -1,42 +1,59 @@
 package prolab2;
 
-import java.sql.Time;
 //Her seferin bir aracı, guzergahı, zamanı, fiyatı gibi bilgiler bulunmalıdır
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Trip {
 		
-		private Time departureTime;
-		private int price;
+    public static Map<String, Integer> ticketBus = new HashMap<>();
+    public static Map<String, Integer> ticketTrain = new HashMap<>();
+    public static Map<String, Integer> ticketAirplane = new HashMap<>();
 
-	    
-	    public Trip(Time departureTime, int price) {
-			this.departureTime = departureTime;
-			this.price = price;
-		}
 
-	    public Time getDepartureTime() {
-			return departureTime;
-		}
-		public void setDepartureTime(Time departureTime) {
-			this.departureTime = departureTime;
-		}
-
-		public int getPrice() {
-			return price;
-		}
-		public void setPrice(int price) {
-			this.price = price;
-		}
-
+	
 		public static void main(String[] args) {
-	    	
-			 new Route(new ArrayList<String>(List.of("Demiryolu", "Istanbul", "Kocaeli", "Bilecik", "Eskisehir", "Ankara")));
-		     new Route(new ArrayList<String>(List.of("Demiryolu", "Istanbul", "Kocaeli", "Bilecik", "Eskisehir", "Konya")));
-		     new Route(new ArrayList<String>(List.of("Karayolu", "Istanbul", "Kocaeli", "Ankara")));
-		     new Route(new ArrayList<String>(List.of("Karayolu", "Istanbul", "Kocaeli", "Eskisehir", "Konya")));
-		     new Route(new ArrayList<String>(List.of("Havayolu", "Istanbul", "Konya")));
-		     new Route(new ArrayList<String>(List.of("Havayolu", "Istanbul", "Ankara")));
+				    	
+			new Route(new ArrayList<String>(List.of("Demiryolu", "Istanbul", "Kocaeli", "Bilecik", "Eskisehir", "Ankara")));
+		    new Route(new ArrayList<String>(List.of("Demiryolu", "Istanbul", "Kocaeli", "Bilecik", "Eskisehir", "Konya")));
+		    new Route(new ArrayList<String>(List.of("Karayolu", "Istanbul", "Kocaeli", "Ankara")));
+		    new Route(new ArrayList<String>(List.of("Karayolu", "Istanbul", "Kocaeli", "Eskisehir", "Konya")));
+		    new Route(new ArrayList<String>(List.of("Havayolu", "Istanbul", "Konya")));
+		    new Route(new ArrayList<String>(List.of("Havayolu", "Istanbul", "Ankara")));
+		    
+		    ticketBus.put("İstanbul-Kocaeli", 50);
+		    ticketBus.put("İstanbul-Eskisehir", 150);
+		    ticketBus.put("İstanbul-Ankara", 300);
+		    ticketBus.put("İstanbul-Konya", 300);
+		    ticketBus.put("Kocaeli-Eskisehir", 100);
+		    ticketBus.put("Kocaeli-Ankara", 400);
+		    ticketBus.put("Kocaeli-Konya", 250);
+		    ticketBus.put("Eskisehir-Konya", 150);
+		    
+		    ticketTrain.put("Istanbul-Kocaeli", 50);
+		    ticketTrain.put("Istanbul-Bilecik", 150);
+		    ticketTrain.put("Istanbul-Eskisehir", 200);
+		    ticketTrain.put("Istanbul-Ankara", 250);
+		    ticketTrain.put("Istanbul-Konya", 300);
+		    ticketTrain.put("Kocaeli-Bilecik", 50);
+		    ticketTrain.put("Kocaeli-Eskisehir", 100);
+		    ticketTrain.put("Kocaeli-Ankara", 200);
+		    ticketTrain.put("Kocaeli-Konya", 250);
+		    ticketTrain.put("Bilecik-Eskisehir", 50);
+		    ticketTrain.put("Bilecik-Ankara", 150);
+		    ticketTrain.put("Bilecik-Konya", 200);
+		    ticketTrain.put("Eskisehir-Ankara", 100);
+		    ticketTrain.put("Eskisehir-Konya", 150);
+		    
+		    ticketAirplane.put("Istanbul-Ankara", 1000);
+		    ticketAirplane.put("Istanbul-Konya", 1200);
+
+
+
+		    
+
+
 	}
 }
