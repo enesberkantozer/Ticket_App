c1e03a3e47dda685b7782ba46f7e70d102e3a8db
Enes Berkant Özer <enesberkantozer260@gmail.com>
2023-12-04 15:01:29 +0300

    Create AddPassengerDialog and event added for buttons

--- LabPro2/src/view/VehicleTemplate.java
+++ LabPro2/src/view/VehicleTemplate.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -33,6 +35,16 @@ public class VehicleTemplate {
 			}
 			
 		});
+		for (int i = 0; i < buttons.size(); i++) {
+			buttons.get(i).addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					AddPassengerDialog.main(null);
+				}
+			});
+		}
 	}
 	
 	public void execute() {
